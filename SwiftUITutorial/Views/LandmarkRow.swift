bcfb7d652231e6fa1029edf4a6c387ea938974e4
Em1rio <48679388+Em1rio@users.noreply.github.com>
2024-04-23 17:17:08 +0300

    Добавил LandmarkRow и отобразил данные из модели

---
+++ SwiftUITutorial/Views/LandmarkRow.swift
@@ -0,0 +1,25 @@
+//
+//  LandmarkRow.swift
+//  SwiftUITutorial
+//
+//  Created by Emir Nasyrov on 23.04.2024.
+//
+
+import SwiftUI
+
+struct LandmarkRow: View {
+    var landmark: Landmark
+    var body: some View {
+        HStack {
+            landmark.image
+                .resizable()
+                .frame(width: 50, height: 50)
+            Text(landmark.name)
+            Spacer()
+        }
+    }
+}
+
+#Preview {
+    LandmarkRow(landmark: landmarks[0])
+}
