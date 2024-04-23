bc3e18da19fd9f2bb428c4e76a58d0a4d4fbc611
Em1rio <48679388+Em1rio@users.noreply.github.com>
2024-04-23 17:02:45 +0300

    Дoбавил MapView

---
+++ SwiftUITutorial/MapView.swift
@@ -0,0 +1,25 @@
+//
+//  MapView.swift
+//  SwiftUITutorial
+//
+//  Created by Emir Nasyrov on 23.04.2024.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: View {
+    var body: some View {
+        Map(initialPosition: .region(region))
+    }
+    private var region: MKCoordinateRegion {
+        MKCoordinateRegion(
+            center: CLLocationCoordinate2D(latitude: 34.011_286, longitude: -116.166_868),
+            span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2)
+        )
+    }
+}
+
+#Preview {
+    MapView()
+}
