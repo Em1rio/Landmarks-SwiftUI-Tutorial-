c5f0cb4640829fe14ca792685c66c23d279a885e
Em1rio <48679388+Em1rio@users.noreply.github.com>
2024-04-23 19:52:03 +0300

    Создал 2 папки и переместил файлы по ним

---
+++ SwiftUITutorial/Views/Helpers/FavoriteButton.swift
@@ -0,0 +1,25 @@
+//
+//  FavoriteButton.swift
+//  SwiftUITutorial
+//
+//  Created by Emir Nasyrov on 23.04.2024.
+//
+
+import SwiftUI
+
+struct FavoriteButton: View {
+    @Binding var isSet: Bool
+    var body: some View {
+        Button {
+            isSet.toggle()
+        } label: {
+            Label("Toggle Favorite", systemImage: isSet ? "star.fill" : "star")
+                .labelStyle(.iconOnly)
+                .foregroundStyle(isSet ? .yellow : .gray)
+        }
+    }
+}
+
+#Preview {
+    FavoriteButton(isSet: .constant(true))
+}
