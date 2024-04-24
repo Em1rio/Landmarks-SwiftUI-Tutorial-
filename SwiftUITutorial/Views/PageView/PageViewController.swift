14706269b35d27e4c52a8f591dfa1c52bbab36b9
Em1rio <48679388+Em1rio@users.noreply.github.com>
2024-04-24 19:35:13 +0300

    Настраивал PageView для отображения PageVIewController

--- SwiftUITutorial/Views/PageView/PageViewController.swift
+++ SwiftUITutorial/Views/PageView/PageViewController.swift
@@ -5,4 +5,28 @@
 //  Created by Emir Nasyrov on 24.04.2024.
 //
 
-import Foundation
+import SwiftUI
+import UIKit
+
+
+struct PageViewController<Page: View>: UIViewControllerRepresentable {
+    var pages: [Page]
+    
+    func makeUIViewController(context: Context) -> UIPageViewController {
+            return getVC()
+        }
+    func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
+           pageViewController.setViewControllers(
+               [UIHostingController(rootView: pages[0])], direction: .forward, animated: true)
+       }
+    
+    private func getVC() -> UIPageViewController {
+        let pageViewController = UIPageViewController(
+            transitionStyle: .scroll,
+            navigationOrientation: .horizontal)
+
+
+        return pageViewController
+    }
+
+}
