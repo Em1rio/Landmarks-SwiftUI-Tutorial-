7869afced961b5b8d22a890d531a38e84807ef93
Em1rio <48679388+Em1rio@users.noreply.github.com>
2024-04-24 17:04:18 +0300

    Создал CategoryRow

---
+++ SwiftUITutorial/Views/Categories/CategoryItem.swift
@@ -0,0 +1,31 @@
+//
+//  CategoryItem.swift
+//  SwiftUITutorial
+//
+//  Created by Emir Nasyrov on 24.04.2024.
+//
+
+import SwiftUI
+
+struct CategoryItem: View {
+    var landmark: Landmark
+    
+    
+    var body: some View {
+        VStack(alignment: .leading) {
+            landmark.image
+                .resizable()
+                .frame(width: 155, height: 155)
+                .cornerRadius(5)
+            Text(landmark.name)
+                .font(.caption)
+        }
+        .padding(.leading, 15)
+    }
+}
+
+
+#Preview {
+    CategoryItem(landmark: ModelData().landmarks[0])
+    
+}
